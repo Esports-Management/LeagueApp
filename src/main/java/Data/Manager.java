30f0f0e5f4d5981804e3a174b720b37032c0c925
Roberts Sīlis <roberts.siilis@gmail.com>
2022-12-06 13:53:27 +0200

    Added constructors, getters and setters

--- src/main/java/Data/Manager.java
+++ src/main/java/Data/Manager.java
@@ -2,4 +2,15 @@ package Data;
 
 public class Manager extends User{
     Team team;
+
+    public Manager() {
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
 }
--- src/main/java/Data/User.java
+++ src/main/java/Data/User.java
@@ -4,4 +4,31 @@ public class User {
     String name;
     String lastName;
     String userName;
+
+    public User() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 }
