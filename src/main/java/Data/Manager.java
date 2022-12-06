0b25ed76950ec4c7443a5fd15816a194d8e1c438
flackx <brendons.liflands@va.lv>
2022-12-06 14:02:41 +0200

    Fix

--- src/main/java/Data/Manager.java
+++ src/main/java/Data/Manager.java
@@ -3,11 +3,7 @@ package Data;
 public class Manager extends User{
     Team team;
 
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-=======
->>>>>>> Stashed changes
+
     public Manager() {
     }
 
@@ -18,8 +14,5 @@ public class Manager extends User{
     public void setTeam(Team team) {
         this.team = team;
     }
-<<<<<<< Updated upstream
-=======
->>>>>>> 30f0f0e5f4d5981804e3a174b720b37032c0c925
->>>>>>> Stashed changes
+
 }
