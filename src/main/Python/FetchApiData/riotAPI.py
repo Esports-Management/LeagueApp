1c878d9347cdd16fd0626dc3bff6ba543638ab2f
flackx <brendons.liflands@va.lv>
2022-12-08 19:30:11 +0200

    Password requirements made, and it checking repeat password

--- src/main/Python/FetchApiData/riotAPI.py
+++ src/main/Python/FetchApiData/riotAPI.py
@@ -9,7 +9,7 @@ class Summoner:
         self.id = summoner['id']
         self.account_id = summoner['accountId']
         self.level = summoner['summonerLevel']
-        self.last_updated = datetime.fromtimestamp(summoner['revisionDate']/1000)
+        self.last_updated = datetime.fromtimestamp(summoner['revisionDate'] / 1000)
         self.puuid = summoner['puuid']
         self.queue = {}
         self.league_names = []
@@ -18,10 +18,12 @@ class Summoner:
         self.match_list = lol_watcher.match.matchlist_by_puuid(region=region, puuid=self.puuid)
 
     def get_challenger_by_queue_ranked_solo_5x5(self, lol_watcher, region):
-        self.queue.update({"RANKED_SOLO_5x5": lol_watcher.league.challenger_by_queue(region=region, queue="RANKED_SOLO_5x5")})
+        self.queue.update(
+            {"RANKED_SOLO_5x5": lol_watcher.league.challenger_by_queue(region=region, queue="RANKED_SOLO_5x5")})
 
     def get_challenger_by_queue_ranked_flex_sr(self, lol_watcher, region):
-        self.queue.update({"RANKED_FLEX_SR": lol_watcher.league.challenger_by_queue(region=region, queue="RANKED_FLEX_SR")})
+        self.queue.update(
+            {"RANKED_FLEX_SR": lol_watcher.league.challenger_by_queue(region=region, queue="RANKED_FLEX_SR")})
 
     def get_queue(self, lol_watcher, region):
         self.queue.update({'all queues': lol_watcher.league.challenger_by_queue(region=region, queue="CUSTOM_GAME")})
@@ -35,20 +37,30 @@ class Summoner:
         match_statistics = {}
 
         for i in range(len(match_metadata['info']['participants'])):
-
             match_statistics.update({match_metadata['info']['participants'][i]['summonerName']: {}})
             match_statistics[match_metadata['info']['participants'][i]['summonerName']]['matchId'] = match_id
-            match_statistics[match_metadata['info']['participants'][i]['summonerName']]['kills'] = match_metadata['info']['participants'][i]['kills']
-            match_statistics[match_metadata['info']['participants'][i]['summonerName']]['deaths'] = match_metadata['info']['participants'][i]['deaths']
-            match_statistics[match_metadata['info']['participants'][i]['summonerName']]['assists'] = match_metadata['info']['participants'][i]['assists']
-            match_statistics[match_metadata['info']['participants'][i]['summonerName']]['firstBloodKill'] = match_metadata['info']['participants'][i]['firstBloodKill']
-            match_statistics[match_metadata['info']['participants'][i]['summonerName']]['goldEarned'] = match_metadata['info']['participants'][i]['goldEarned']
-            match_statistics[match_metadata['info']['participants'][i]['summonerName']]['pentaKills'] = match_metadata['info']['participants'][i]['pentaKills']
-            match_statistics[match_metadata['info']['participants'][i]['summonerName']]['timeCCingOthers'] = match_metadata['info']['participants'][i]['timeCCingOthers']
-            match_statistics[match_metadata['info']['participants'][i]['summonerName']]['totalTimeCCDealt'] = match_metadata['info']['participants'][i]['totalTimeCCDealt']
-            match_statistics[match_metadata['info']['participants'][i]['summonerName']]['totalDamageDealtToChampions'] = match_metadata['info']['participants'][i]['totalDamageDealtToChampions']
-            match_statistics[match_metadata['info']['participants'][i]['summonerName']]['totalMinionsKilled'] = match_metadata['info']['participants'][i]['totalMinionsKilled']
-            match_statistics[match_metadata['info']['participants'][i]['summonerName']]['visionScore'] = match_metadata['info']['participants'][i]['visionScore']
+            match_statistics[match_metadata['info']['participants'][i]['summonerName']]['kills'] = \
+            match_metadata['info']['participants'][i]['kills']
+            match_statistics[match_metadata['info']['participants'][i]['summonerName']]['deaths'] = \
+            match_metadata['info']['participants'][i]['deaths']
+            match_statistics[match_metadata['info']['participants'][i]['summonerName']]['assists'] = \
+            match_metadata['info']['participants'][i]['assists']
+            match_statistics[match_metadata['info']['participants'][i]['summonerName']]['firstBloodKill'] = \
+            match_metadata['info']['participants'][i]['firstBloodKill']
+            match_statistics[match_metadata['info']['participants'][i]['summonerName']]['goldEarned'] = \
+            match_metadata['info']['participants'][i]['goldEarned']
+            match_statistics[match_metadata['info']['participants'][i]['summonerName']]['pentaKills'] = \
+            match_metadata['info']['participants'][i]['pentaKills']
+            match_statistics[match_metadata['info']['participants'][i]['summonerName']]['timeCCingOthers'] = \
+            match_metadata['info']['participants'][i]['timeCCingOthers']
+            match_statistics[match_metadata['info']['participants'][i]['summonerName']]['totalTimeCCDealt'] = \
+            match_metadata['info']['participants'][i]['totalTimeCCDealt']
+            match_statistics[match_metadata['info']['participants'][i]['summonerName']]['totalDamageDealtToChampions'] = \
+            match_metadata['info']['participants'][i]['totalDamageDealtToChampions']
+            match_statistics[match_metadata['info']['participants'][i]['summonerName']]['totalMinionsKilled'] = \
+            match_metadata['info']['participants'][i]['totalMinionsKilled']
+            match_statistics[match_metadata['info']['participants'][i]['summonerName']]['visionScore'] = \
+            match_metadata['info']['participants'][i]['visionScore']
 
         return match_statistics
 
