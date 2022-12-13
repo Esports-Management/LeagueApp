c52b452b775a7f51e630eb6f6ec6c8fc60933cb5
flackx <brendons.liflands@va.lv>
2022-12-13 11:14:54 +0200

    Fixed config template

--- src/main/Python/Conf/configtemplate.py
+++ src/main/Python/Conf/configtemplate.py
@@ -2,15 +2,15 @@ import mysql.connector
 from mysql.connector.constants import ClientFlag
 
 
-class config:
-    def getconfig(self):
-        cnx = mysql.connector.connect(host="localhost",
-                                      user="",
-                                      password="",
-                                      database="",
-                                      port = '',)
-        return cnx
+def getapikey():
+    api_key = 'RGAPI-9d25c1b9-0261-47b9-a248-66de594c6ac6'
+    return api_key
 
-    def getapikey(self):
-        api_key = ''
-        return api_key
+
+def getconfig():
+    cnx = mysql.connector.connect(host="localhost",
+                                  user="root",
+                                  password="",
+                                  database="EsportsApp",
+                                  port = '3306',)
+    return cnx
